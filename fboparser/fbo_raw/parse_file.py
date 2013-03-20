fec6f163cd016d2962839f7cbd0741e828405c84
Drew Vogel <dvogel@sunlightfoundation.com>
2013-03-20 12:25:53 -0400

    Changed parser to be invokable via the command line.

--- fboparser/fbo_raw/parse_file.py
+++ fboparser/fbo_raw/parse_file.py
@@ -1,9 +1,6 @@
-from bs4 import BeautifulSoup
+from __future__ import print_function
+import sys
 from BeautifulSoup import BeautifulSoup as BS
-
-filename = '/home/kaitlin/envs/fboparser/raw_files/FBOFeed20120412'
-
-f = open(filename).read()
 
 #tags that appear inside the top level tags and aren't closed
 broken_tags=['date', 'year', 'agency', 'zip','classcod','naics','offadd','subject','solnbr','respdate','archdate','contact','desc','link','email','links','files','setaside','popaddress','popzip','popcountry','recovery_act','ntype','awdnbr','awddate','awdamt','awardee','awardee_duns','modnbr','donbr','foja', 'office', 'location', 'stauth', 'url']
@@ -12,17 +9,19 @@ broken_tags=['date', 'year', 'agency', 'zip','classcod','naics','offadd','subjec
 top_level_tags = ['presol', 'combine', 'award', 'ja', 'fairopp', 'mod', 'archive', 'unarchive']
 
 #have beautifulsoup fix unclosed tags inside main nodes
-for bt in broken_tags:
-    BS.RESET_NESTING_TAGS[bt] = top_level_tags
-    BS.NESTABLE_TAGS[bt] = top_level_tags
 
-#parse actual text
-soup = BS(f)
+def parse_file(path):
+    with file(path, 'rb') as f:
+        return parse_stream(f)
 
-#print some stuff
-print soup.presol.prettify()
-print soup.presol.date.prettify()
-print soup.presol.year.prettify()
+def parse_stream(fobj):
+    for bt in broken_tags:
+        BS.RESET_NESTING_TAGS[bt] = top_level_tags
+        BS.NESTABLE_TAGS[bt] = top_level_tags
+    soup = BS(fobj)
+    return soup
 
-#g.write(soup.prettify().encode('utf-8'))
-
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        feed = parse_file(sys.argv[1])
+        print(feed.prettify())
